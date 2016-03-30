eed3851cc5ecc4101ccb0bc6ab5fa3c1eb4e90c0
Chad Whitacre <chad@zetaweb.com>
2016-03-30 15:03:53 -0400

    Bring over the tally script from #3807

---
+++ tally-backfill.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import commands
+
+
+def title(title):
+    print(title)
+    print("-"*34)
+
+def report(*patterns):
+    N = 0
+    for pattern in patterns:
+        n = int(commands.getoutput('grep "{}$" backfill.log | wc -l'.format(pattern)))
+        N += n
+        print("{:<28} {:>5}".format(pattern, n))
+    print("{:<28} {:>5}".format('', N))
+
+
+report( 'IMPOSSIBLE!!!!!!!!!!!'
+      , 'all set!'
+      , 'yes'
+      , 'no'
+      , 'missing ref and status!'
+      , 'mismatched ref and status!'
+      , 'missing ref!'
+      , 'mismatched ref!'
+      , 'missing status!'
+      , 'mismatched status!'
+       )
