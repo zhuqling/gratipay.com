247dbe52a711f5fc8568c4e139282b61ea08bac1
Chad Whitacre <chad@zetaweb.com>
2016-04-21 16:54:08 -0400

    Kill the rest of the old code

--- match-balanced.py
+++ match-balanced.py
@@ -7,7 +7,6 @@ import os
 import time
 from collections import defaultdict
 from decimal import Decimal as D
-from os import path
 
 import psycopg2.tz
 from gratipay import wireup
@@ -19,25 +18,12 @@ header = lambda h: print(h.upper() + ' ' + ((80 - len(h) - 1) * '-'))
 class Heck(Exception): pass
 
 
-FULL = """\
-
-        SELECT e.*, p.id as user_id
-          FROM exchanges e
-          JOIN participants p
-            ON e.participant = p.username
-         WHERE substr("timestamp"::text, 0, 8) = %s
-           AND recorder IS NULL -- filter out PayPal
-      ORDER BY "timestamp" asc
-
-"""
-
-
 def datetime_from_iso(iso):
-    date, time = iso.split('T')
-    assert time[-1] == 'Z'
-    time = time[:-1]
+    date, time_ = iso.split('T')
+    assert time_[-1] == 'Z'
+    time_ = time_[:-1]
     year, month, day = map(int, date.split('-'))
-    hour, minute, second_microsecond = time.split(':')
+    hour, minute, second_microsecond = time_.split(':')
     hour, minute = map(int, (hour, minute))
     second, microsecond = map(int, second_microsecond.split('.'))
     tz = psycopg2.tz.FixedOffsetTimezone(offset=0, name=None)
@@ -63,144 +49,6 @@ def ts_within(transaction, exchange, seconds):
     ts_exchange = exchange.timestamp
     limit = ts_transaction + datetime.timedelta(seconds=seconds)
     return ts_exchange <= limit
-
-
-def process_month(matcher, year, month):
-    input_csv = path.join('3912', year, month, '_balanced.csv')
-    match_csv = path.join('3912', year, month, 'balanced')
-    if not path.isfile(input_csv): return
-    reader = csv.reader(open(input_csv))
-    writer = csv.writer(open(match_csv, 'w+'))
-
-    matcher.load_month(year, month)
-
-    headers = next(reader)
-    rec2mat = {}
-    inexact = []
-    ordered = []
-    failed = set()
-
-    header("FINDING")
-    for row in reader:
-        rec = dict(zip(headers, row))
-
-        log = lambda *a, **kw: print(rec['created_at'], *a, **kw)
-
-        cid = rec['links__customer']
-        ordered.append(rec)
-
-        match = matcher.find(log, rec['created_at'], rec['amount'], rec['description'])
-        if match:
-            uid = match.user_id
-            known = matcher.uid2cid.get(uid)
-            if known:
-                assert cid == known, (rec, match)
-            else:
-                matcher.uid2cid[uid] = cid
-                matcher.cid2uid[cid] = uid
-            rec2mat[rec['id']] = match
-
-            if match.route is not None:
-                if match.ref is None and match.status is None:
-                    print('missing ref and status!')
-                elif match.ref != rec['id'] and match.status != rec['status']:
-                    print('mismatched ref and status!')
-                elif match.ref is None:
-                    print('missing ref!')
-                elif match.ref != rec['id']:
-                    print('mismatched ref!')
-                elif match.status is None:
-                    print('missing status!')
-                elif match.status != rec['status']:
-                    print('mismatched status!')
-                else:
-                    ordered.pop()
-                    print('all set!')
-            else:
-                print('yes')
-        else:
-            inexact.append(rec)
-            print('no')
-
-    header("FUZZING")
-    for rec in inexact:
-        cid = rec['links__customer']
-        rid = rec['id']
-        guess = matcher.cid2uid.get(cid)
-        possible = matcher.fuzz(log, rec['created_at'], rec['amount'], guess, rec['description'])
-        npossible = len(possible)
-
-        def fail(msg):
-            print(msg)
-            failed.add(rid)
-
-        print(' => ', end='')
-
-        if guess:
-            if npossible == 0:
-                fail('Eep! Guess failed!')
-            elif npossible > 1:
-                fail('What?! Too many!')
-            else:
-                match = possible[0]
-                print(match.participant)
-        elif not possible:
-            fail(' ... IMPOSSIBLE!!!!!!!!!!!')
-        else:
-            mindelta = None
-
-            date, time = rec['created_at'].split('T')
-            Y,M,D = date.split('-')
-            h,m,s = time.split(':')
-            s,ms = s.split('.')
-            ms = ms[:-1]
-            Y,M,D,h,m,s,ms = [int(x) for x in (Y,M,D,h,m,s,ms)]
-            ts_balanced = datetime.datetime(Y,M,D,h,m,s,ms, possible[0].timestamp.tzinfo)
-
-            for p in possible:
-                delta = abs(ts_balanced - p.timestamp)
-                if mindelta is None or (delta < mindelta):
-                    mindelta = delta
-                    match = p
-
-            matcher.cid2uid[cid] = match.user_id
-            possible.remove(match)
-            print(match.participant, 'INSTEAD OF', ' OR '.join([p.participant for p in possible]))
-
-        if rid in failed:
-            continue
-
-        rec2mat[rid] = match
-
-    header("WRITING")
-    for rec in ordered:
-        if rec['id'] in failed: continue
-        match = rec2mat.get(rec['id'])
-        if match is None:
-            assert rec['status'] == 'failed', rec['id']
-            writer.writerow([ match.participant
-                            , match.user_id
-                            , rec['links__customer']
-                            , ''
-                            , rec['created_at']
-                            , rec['amount']
-                            , rec['id']
-                            , rec['status']
-                             ])
-        else:
-            writer.writerow([ match.participant
-                            , match.user_id
-                            , rec['links__customer']
-                            , match.id
-                            , ''
-                            , ''
-                            , rec['id']
-                            , rec['status']
-                             ])
-
-
-### OLD ^^^^^^^^^^^^^^^^^^
-### NEW vvvvvvvvvvvvvvvvvv
 
 
 def get_exchanges(db):
@@ -389,6 +237,22 @@ class Matcher(object):
                     matched_t.add(t['id'])
                     matched_e.add(e.id)
                     self.matches.append((t, e))
+
+                    if 0:
+                        # XXX Bring me back!
+                        if e.ref is None and e.status is None:
+                            print('missing ref and status!')
+                        elif e.ref != t['id'] and e.status != t['status']:
+                            print('mismatched ref and status!')
+                        elif e.ref is None:
+                            print('missing ref!')
+                        elif e.ref != t['id']:
+                            print('mismatched ref!')
+                        elif e.status is None:
+                            print('missing status!')
+                        elif e.status != t['status']:
+                            print('mismatched status!')
+
                     break
 
         self.uncategorized['transactions'] += [t for t in transactions if t['id'] not in matched_t]
